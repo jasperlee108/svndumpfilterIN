1f0e27969b2021d440f77bc0514daeb9cb67c8c8
amac <freeamac@gmail.com>
2024-02-29 12:14:53 -0800

    Support Python3 (#37)
    * Convert to python 3.10
    
    Initial 2to3 conversion.
    
    Convert to using pycodestyle and update the
    docs from pep8 to pycodestyle.
    
    * Fix test to not rely on list order
    
    * Explicity use python3 executable
    
    Update documentation to use python3. Also use markdown links.
    
    * Update requirements swapping pep8 with pycodestyle
    
    * Update getrev.py script to python3
    
    * Add github actions workflow file
    
    * Fix code style
    
    * Ensure all python files are checked for codestyle errors
    
    * Update to python 3
    
    * Change all static strings to use single quotes
    * Change all print statements to use format instead of %
    * Handle the mixed binary and text data found in a svndump file
    * Write out new dump file as binary
    * Fix test error where result order can be correct but arbitrary causing a failure
    * Update docs
    
    * Fix codestyle issues
    
    * Add in Bandit for workflow run
    
    * Fixes for bandit errors
    
    ---------
    
    Co-authored-by: amac <amac@employees.org>
    Co-authored-by: amac <5177663+freeamac@users.noreply.github.com>

--- test_svndumpfilter.py
+++ test_svndumpfilter.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from svndumpfilter import *
 
@@ -12,7 +12,7 @@ def path_match_exclude(expected, excluded):
     check = MatchFiles(False)
     for item in excluded:
         check.add_to_matches(item)
-    for path, result in expected.iteritems():
+    for path, result in iter(expected.items()):
         assert check.is_included(path) == result
     return True
 
@@ -24,7 +24,7 @@ def path_match_include(expected, included):
     check = MatchFiles(True)
     for item in included:
         check.add_to_matches(item)
-    for path, result in expected.iteritems():
+    for path, result in iter(expected.items()):
         assert check.is_included(path) == result
 
 
@@ -119,4 +119,4 @@ def test_add_dependents_3():
         add_dependents(to_write, check.matches, dump_version)
         actual_paths = [node.head["Node-path"] for node in to_write]
         expected_paths = ["whoop", "coconut", "whoop/whoo"]
-        assert actual_paths == expected_paths
+        assert set(actual_paths) == set(expected_paths)
